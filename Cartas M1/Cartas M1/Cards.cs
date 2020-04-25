0899851519bbfe6fa3a2bc51f250e07d16d5bd62
Dragvid <55921056+Dragvid@users.noreply.github.com>
2020-04-25 15:38:53 +0100

    upload do progresso do m1

---
+++ Cartas M1/Cartas M1/Cards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartas_M1
+{
+    public class Cards
+    {
+        public string Name { get; set; }
+        public string effect { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public int range { get; set; }
+        public int atack { get; set; }
+    }
+}
---
+++ Cartas M1/Cartas M1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartas_M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = File.ReadAllText("Cartas.json");
+            Dictionary<string, List<Cards>> cards = JsonConvert.DeserializeObject<Dictionary<string, List<Cards>>>(content);
+            foreach (KeyValuePair<string, List<Cards>> item in cards)
+            {
+                Console.WriteLine("Card type: "+item.Key);
+                foreach (Cards crd in item.Value)
+                {
+                    Console.WriteLine("name: "+crd.Name);
+                    
+                    if(item.Key== "character")
+                    {
+                        Console.WriteLine("health: " + crd.health);
+                        Console.WriteLine("Atk: " + crd.atack);
+                        Console.WriteLine("Speed: " + crd.speed);
+                    }
+                    if (item.Key == "Prop")
+                    {
+                        Console.WriteLine("health: " + crd.health);
+                    }
+                    Console.WriteLine("effect: " + crd.effect);
+                    Console.WriteLine("\n");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
